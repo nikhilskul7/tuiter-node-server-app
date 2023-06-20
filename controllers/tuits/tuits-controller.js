84b844447bc393043817fb5ff08716e6af7f9741
Nikhil Kulkarni <nikhilskul7@gmail.com>
2023-06-20 18:13:02 -0400

    Minor bugs

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,9 +1,31 @@
-import * as tuitsDao from './tuits-dao.js'
+import * as tuitsDao from './tuits-dao.js';
 
-
-
-
-
+const createTuit = async(req, res) => {
+    const newTuit = req.body;
+    newTuit.likes = 0;
+    newTuit.dislikes = 0;
+    newTuit.replies = 0;
+    newTuit.retuits = 0;
+    newTuit.time = "1s";
+    newTuit.liked = false;
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
+}
+const findTuits = async(req, res) => {
+    const tuits = await tuitsDao.findTuits();
+    res.json(tuits);
+}
+const updateTuit = async(req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+    res.json(status);
+}  
+const deleteTuit = async(req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+    res.json(status);
+}
 
 export default (app) => {
  app.post('/api/tuits', createTuit);
@@ -11,34 +33,3 @@ export default (app) => {
  app.put('/api/tuits/:tid', updateTuit);
  app.delete('/api/tuits/:tid', deleteTuit);
 }
-const findTuits =async (req, res) =>{const tuits = await tuitsDao.findTuits()
-  res.json(tuits);}
-
-
-
-  const createTuit = async (req, res) => {
-    const newTuit = req.body;
-  
-    newTuit.likes = 0;
-    newTuit.liked = false;
-    
-    const insertedTuit = await tuitsDao.createTuit(newTuit);
-    res.json(insertedTuit);
-  }
-  
-  const deleteTuit = async (req, res) => {
-    const tuitdIdToDelete = req.params.tid;
-    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-    
-    res.json(status);
-  }
-  
-  const updateTuit = async (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const updates = req.body;
-    const status = await tuitsDao
-                       .updateTuit(tuitdIdToUpdate, updates);
-  res.json(status);
-
-  }
-  
